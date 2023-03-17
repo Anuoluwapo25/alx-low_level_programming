cfd9b222c666e6998c0bbecf640d3b04fccf79d7
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-16 19:06:49 -0700

    ax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a long int: %ul byte(s)\n", (unsigned long)sizeof(long int));
 	printf("Size of a long long int: %ul byte(s)\n", (unsigned long)sizeof(long long int));
 	printf("size of a char int: %ul byte(s)\n", (unsigned long)sizeof(char));
-	return (0)
+	return (0);
 }
 
