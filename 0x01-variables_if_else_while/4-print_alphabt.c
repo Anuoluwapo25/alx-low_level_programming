6286e8f0e012d863c4e90d524182abcc73f408fe
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-17 12:33:12 -0700

    not e nor q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	cha la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if  (la != 'e'; la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
