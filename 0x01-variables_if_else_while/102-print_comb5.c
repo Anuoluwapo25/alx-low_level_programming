25a947324833cea3b4744a741d81449ef9365ddd
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-19 08:25:00 -0700

    two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include<stdio.h
+#include<stdio.h>
 
 /**
  * main - main functio
@@ -6,3 +6,31 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i < 98 || j < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
