5e355ad393b104b6b21c6d1b50e619123d702daf
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-23 16:18:27 -0700

    return if uppercase

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function to check uppercase
+ *
+ * @c: parameter to be checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
