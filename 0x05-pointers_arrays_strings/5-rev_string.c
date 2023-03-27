28b0d86f1d60cf23cf769cc02856f0b8e8db4bde
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-27 16:33:10 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - print rev string
+ *
+ * @s: parameter to rev
+ *
+ * Return: always 0
+ */
+
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0 ; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
+
