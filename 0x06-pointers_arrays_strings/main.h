26547c99d3fef018fff7039b2d6bd773c4e26a49
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-03-30 16:27:28 -0700

    function that encodes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 char *rot13(char *);
 int main(void);
 void print_number(int n);
