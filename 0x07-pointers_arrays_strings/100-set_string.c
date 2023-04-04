b5be2bfeb7487b8eb468f825ea5c4ffd28005907
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-03 21:19:07 -0700

    locate a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - function that set the value of pointer to char
+ *
+ * @s: parameter s
+ * @to: parameter to
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
