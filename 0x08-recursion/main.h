4e6aaf84aa42d8d321b179c67c5b02fee97887a3
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-05 21:09:44 -0700

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+
+#endif
