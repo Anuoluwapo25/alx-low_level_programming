42b2a12dd26a75912ec216b8507b2e942ea4a471
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-17 11:01:23 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef SIZE
+#define SIZE 1024
+#include <stdio.h>
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
+
