ef6a6d254092559d6c8a428d70cf016798f17219
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-19 07:38:59 -0700

    my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "function_pointers.h"
 
 /**
  * print_name - function that prints name
@@ -10,6 +11,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name == NULL || f == NULL)
+		printf("Error");
+	else
+		f(name);
 }
 
