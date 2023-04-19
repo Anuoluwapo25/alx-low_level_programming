904c85ff165c6665bfefb2dafbcf79cf0a860b79
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-19 07:54:16 -0700

    array pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "function_pointers.h"
 
 /**
  * array_iterator - function that excecute a function
@@ -11,3 +12,10 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	long unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTER		
 #define FUNCTION_POINTER
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
