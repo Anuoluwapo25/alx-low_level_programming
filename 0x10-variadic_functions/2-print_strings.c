912b1d23dca4631da69274d610d6563f7d49d962
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-20 22:11:22 -0700

    string print

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - strings
+ *
+ * @separator: string s
+ * @n: num of string
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list mystr;
+
+	va_start(mystr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(mystr, char*);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		printf("\n");
+		va_end(mystr);
+	}
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,9 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
