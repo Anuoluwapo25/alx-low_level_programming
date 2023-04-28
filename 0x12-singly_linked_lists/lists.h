a050ae07dca8043531e9a31175817934a7387592
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-27 21:49:43 -0700

    tests.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LIST_H
-#define LiST_H
+
+#define LIST_H
+#define LIST_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,11 +14,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t list_len(const list_t *h);
@@ -25,4 +29,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void __attribute__ ((constructor)) first(void);
 
-#endif
+#endif /* LIST_H */
