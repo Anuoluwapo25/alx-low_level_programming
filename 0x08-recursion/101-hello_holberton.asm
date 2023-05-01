fb2835acc4d0ae4a120c7f31a008561f66fdabf6
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-04-30 18:41:22 -0700

    assemby

--- 0x08-recursion/101-hello_holberton.asm
+++ 0x08-recursion/101-hello_holberton.asm
@@ -1,14 +1,19 @@
-message db 'Hello, Holberton', 0Ah, 0
+
+section .data
+    hello: db "Hello, Holberton", 10, 0  ; 10 is the ASCII code for newline
 
 section .text
     global main
     extern printf
 
 main:
-    push message
-    mov rdi, message
-    xor eax, eax
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16  ; allocate space on the stack for printf arguments
+    mov rdi, hello  ; first argument: pointer to the format string
+    xor eax, eax  ; clear the upper 32 bits of rax
     call printf
-    add rsp, 8
-    xor eax, eax
+    add rsp, 16  ; deallocate the stack space
+    mov rsp, rbp
+    pop rbp
     ret
