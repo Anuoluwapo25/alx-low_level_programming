b732d353126dcb9465fb50c8caa1f613209290cb
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-05-02 03:58:33 -0700

    index

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTER
 #include <stdio.h>
 
+int int_index(int *array, int size, int (*cmp)(int))
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
