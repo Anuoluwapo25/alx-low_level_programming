3659a7c5e2e3bccfcf61d299e59dcf18a1fea719
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-05-09 03:20:49 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - finction that creates afile
+ *
+ * @filename: name of file
+ * @text_content: text context
+ *
+ * Return: 1 and -1 on sucess
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fh;
+
+	if (filename == NULL)
+		return (-1);
+	fh = fopen(filename, "w");
+	if (fh == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL || *text_content == '\0')
+	{
+		fclose(fh);
+		return (1);
+	}
+	fwrite(text_content, sizeof(char), strlen(text_content), fh);
+	fclose(fh);
+
+	return (1);
+}
