a84a5d32c8c983cdae5cba4bf4215932c23165d5
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-05-09 05:08:09 -0700

    appending

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * append_text_to_file - function to append
+ *
+ * @filename: name of file
+ * @text_content: NULL terminating character
+ *
+ * Return: 1 and -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fh;
+
+	if (filename == NULL)
+		return (-1);
+	fh = fopen(filename, "a");
+	if (fh == NULL)
+		return (-1);
+	if (text_content == NULL) 
+	{
+		fclose(fh);
+		return (1);
+	}
+	fprintf(fh, "%s", text_content);
+
+	fclose(fh);
+
+	return(1);
+}
