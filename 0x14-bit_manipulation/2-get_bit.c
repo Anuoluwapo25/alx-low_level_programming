d3638a33399a60003e134915838761a4ddf4168d
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-07-07 17:35:49 -0700

    print binary number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - return the value of a bit st index
+ *
+ * @n: number
+ * @index: index to set
+ *
+ * Return: -1 or index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i; 
+
+	if (index > 64)
+	{
+		return (-1);
+	}
+	i = ((n >> index) & 1);
+
+	return (i);
+
+}
