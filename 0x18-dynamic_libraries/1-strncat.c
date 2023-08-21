fb6c21de3fdfc3e4abf6a6e69ee076fcb45102c7
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-08-21 16:38:12 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenate two strings
+ * * @dest: par src
+ * @src: parameter src
+ * @n: parameter n
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int i;
+        int j;
+
+        i = 0;
+
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (j < n)
+        {
+                dest[i] = src[j];
+                if (j == '\0')
+                i++;
+                j++;
+        }
+
+        return (dest);
+}
