e7cce546aa5c36beeab405422df61fb11f780344
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-10-07 03:02:23 -0700

    corrcted main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,12 +8,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void error_exit(int code, const char *message) 
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
-}
-
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
