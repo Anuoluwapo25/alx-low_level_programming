c638ca34f6db6acc36b58c5f3485a3ba85a029d4
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-02 03:14:16 -0800

    create

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,54 +1,81 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table.
- * @key: The key to add or update.
- * @value: The value associated with the key (duplicated).
- *
- * Return: 1 if successful, 0 otherwise.
+ * create_pair - creates a new ke
+ * @key: the key
+ * @value: the value 
+ * Return: new 
+ */
+hash_node_t *create_pair(const char *key, const char *value)
+{
+	hash_node_t *new_pair = NULL;
+
+	if (!key || !value || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	new_pair = malloc(sizeof(hash_node_t));
+	if (new_pair == NULL)
+		return (NULL);
+	new_pair->key = strdup(key);
+	if (new_pair->key == NULL)
+	{
+		free(new_pair);
+		return (NULL);
+	}
+	new_pair->value = strdup(value);
+	if (new_pair->value == NULL)
+	{
+		free(new_pair->key);
+		free(new_pair);
+		return (NULL);
+	}
+	new_pair->next = NULL; 
+
+	return (new_pair);
+}
+/**
+ * hash_table_set - adds an element
+ * @ht: the hash tabl
+ * @key: the key to add 
+ * @value: the value
+ * Return: 1 
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
-	
-	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
-		return 0;
-	
-	index = key_index((const unsigned char *)key, ht->size);
-	
-	current = ht->array[index];
-	
-	while (current != NULL)
+	unsigned long int index = 0;
+	hash_node_t *new_pair = NULL, *curr = NULL;
+	if (!ht || !key || !value || strlen(key) == 0)
+		return (0);
+	new_pair = create_pair(key, value);
+	if (new_pair == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size); 
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_pair;
+	else
 	{
-		if (strcmp(current->key, key) == 0)
+		curr = ht->array[index];
+		for (; curr != NULL; curr = curr->next)
 		{
-			free(current->value);
-			current->value = strdup(value);
-			if (current->value == NULL)
-				return 0;
-			return 1;
+			if (strcmp(curr->key, key) == 0)
+			{
+				free(curr->value);
+				curr->value = strdup(value);
+				if (curr->value == NULL)
+				{
+					free(new_pair->key);
+					free(new_pair->value);
+					free(new_pair);
+					return (0);
+				}
+				free(new_pair->key);
+				free(new_pair->value);
+				free(new_pair);
+				return (1);
+			}
 		}
-		current = current->next;
+		new_pair->next = ht->array[index];
+		ht->array[index] = new_pair;
 	}
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return 0;
-	
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-		return 0;
-	}
-	
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return 1;
+	return (1);
 }
-
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -88,6 +88,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+hash_node_t *create_pair(const char *key, const char *value);
 
 
 #endif
