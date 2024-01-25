845def7d2124a1a72644828adc79373639c82c78
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-24 16:01:16 -0800

    rm

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module that returns the perimeter of the island
+"""
+
+def island_perimeter(grid):
+    """
+    function that returns the perimeter of a island
+    """
+    grid = 
+
+
+island_perimeter(grid)
