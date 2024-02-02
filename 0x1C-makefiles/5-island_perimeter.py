f4c8fb859bb61ef09bd1eb719f905766d3367c7c
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-02 05:17:54 -0800

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,22 @@ def island_perimeter(grid):
     Returns:
          perimeter of thenisland`
     """
-    grid = 
+    if not grid or not grid[0]:
+        return 0
 
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
 
-island_perimeter(grid)
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
