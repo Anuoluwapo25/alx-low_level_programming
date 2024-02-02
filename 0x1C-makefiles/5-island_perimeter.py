7e5353eebf9146c96d8e61e33569d5d962a174f1
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-02 05:12:20 -0800

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,6 +6,9 @@ module that returns the perimeter of the island
 def island_perimeter(grid):
     """
     function that returns the perimeter of a island
+    
+    Returns:
+         perimeter of thenisland`
     """
     grid = 
 
