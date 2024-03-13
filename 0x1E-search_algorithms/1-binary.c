0ab344fd437f00c4b09f979a0f9effd08c50504b
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-03-13 14:19:42 -0700

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,4 +9,33 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	int l = 0;
+	int r = size - 1;
+	int mid, i;
 
+	if (array != NULL)
+	{
+		while (l <= r)
+		{
+			mid = (l + r) / 2;
+			printf("Searching in array: ");
+			for (i = l; i <= r; i++)
+			{
+				printf("%d", array[i]);
+				if (i != r)
+					printf(", ");
+			}
+			printf("\n");
+			if (array[mid] ==  value)
+				return mid;
+			else if (array[mid] < value)
+			{
+				l = mid + 1;
+			}
+			else
+				r = mid - 1;
+		}
+	}
+
+	return (-1);
+}
